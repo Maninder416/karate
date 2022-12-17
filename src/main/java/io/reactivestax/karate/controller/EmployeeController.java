4584d80f0e60ef939156868ff9b685a704855439
Maninder416 <Maninder.singh@fullstacklabs.ca>
2022-12-17 17:57:50 -0500

    added the post endpoint

--- src/main/java/io/reactivestax/karate/controller/EmployeeController.java
+++ src/main/java/io/reactivestax/karate/controller/EmployeeController.java
@@ -1,17 +1,21 @@
 package io.reactivestax.karate.controller;
 
 import io.reactivestax.karate.Model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    private List<Employee> employeeList= Arrays.asList(new Employee(1,"robin"),
-            new Employee(2,"test-user1"));
+    private List<Employee> employeeList= new ArrayList<>();
+
+    public EmployeeController(){
+        employeeList.add(new Employee(1,"robin"));
+        employeeList.add(new Employee(3,"demo"));
+        employeeList.add(new Employee(2,"test-user1"));
+    }
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
@@ -21,5 +25,11 @@ public class EmployeeController {
     @GetMapping("/employees/{id}")
     public Employee getEmployeeById(@PathVariable Integer id){
         return employeeList.get(id);
+    }
+
+    @PostMapping("/employees")
+    public String getAllEmployees(@RequestBody Employee employee){
+        employeeList.add(employee);
+        return "Employee added";
     }
 }
