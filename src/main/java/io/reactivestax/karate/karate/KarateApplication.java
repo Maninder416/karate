3ab9467977dd3757f99d7958791006895eff52ef
Maninder416 <Maninder.singh@fullstacklabs.ca>
2022-12-17 15:31:26 -0500

    adding pom dependencies

---
+++ src/main/java/io/reactivestax/karate/karate/KarateApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.karate.karate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateApplication.class, args);
+	}
+
+}
