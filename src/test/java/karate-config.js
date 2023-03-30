de0445f369f99a4a04a04cce5023deeb8aeebc00
Maninder416 <Maninder.singh@fullstacklabs.ca>
2023-03-30 16:38:19 -0400

    added readme.md file

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,5 +3,6 @@ function fn() {
   var config = {
 	baseUrl: 'http://localhost:9000'
   }
+  karate.configure('driverTarget', { docker: 'ptrthomas/karate-chrome', secComp: 'src/test/java/chrome.json', vncPort: 5900 });
   return config;
 }
